6ba61fb6968562960e7324a21a78326404eb49ba
Sönke Tippelmann <st@local>
2022-09-08 21:35:28 +0200

    Refactor.

--- binary-tree-pruning/src/main/scala/binarytreepruning/BinaryTreePruning.scala
+++ binary-tree-pruning/src/main/scala/binarytreepruning/BinaryTreePruning.scala
@@ -3,21 +3,20 @@ package binarytreepruning
 object BinaryTreePruning {
   case class TreeNode(value: Int, left: Option[TreeNode], right: Option[TreeNode])
 
-  // todo: unclear: what happens if the root node does not contain a 1?
+  // todo: unclear: what should happen if the root node does not contain a 1?
+  // In this case the current code throws an exception
   def run(root: TreeNode): TreeNode = iterate(Some(root)).get
 
   private def iterate(node: Option[TreeNode]): Option[TreeNode] =
-    node match {
-      case None => None
-      case Some(TreeNode(1, l, r)) =>
-        Some(TreeNode(1, iterate(l), iterate(r)))
-      case Some(TreeNode(n, l, r)) =>
-        val newL = iterate(l)
-        val newR = iterate(r)
-        if(newL.isDefined || newR.isDefined)
-          Some(TreeNode(n,newL,newR))
-        else
-          None
-    }
+    node
+      .map {
+        case TreeNode(n, l, r) =>
+          TreeNode(n, iterate(l), iterate(r))
+      }
+      .filter {
+        case TreeNode(1, _, _) => true
+        case TreeNode(_, None, None) => false
+        case _ => true
+      }
 }
 
