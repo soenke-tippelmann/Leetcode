052ae808d0fd9fdf630de380a4f081da8926a181
Sönke Tippelmann <st@local>
2022-09-08 14:37:48 +0200

    Implement solution.

--- binary-tree-pruning/src/main/scala/binarytreepruning/BinaryTreePruning.scala
+++ binary-tree-pruning/src/main/scala/binarytreepruning/BinaryTreePruning.scala
@@ -1,8 +1,23 @@
 package binarytreepruning
 
 object BinaryTreePruning {
-  def TN = TreeNode.apply _
   case class TreeNode(value: Int, left: Option[TreeNode], right: Option[TreeNode])
 
-  def run(root: TreeNode): TreeNode = root
+  // todo: unclear: what happens if the root node does not contain a 1?
+  def run(root: TreeNode): TreeNode = iterate(Some(root)).get
+
+  private def iterate(node: Option[TreeNode]): Option[TreeNode] =
+    node match {
+      case None => None
+      case Some(TreeNode(1, l, r)) =>
+        Some(TreeNode(1, iterate(l), iterate(r)))
+      case Some(TreeNode(n, l, r)) =>
+        val newL = iterate(l)
+        val newR = iterate(r)
+        if(newL.isDefined || newR.isDefined)
+          Some(TreeNode(n,newL,newR))
+        else
+          None
+    }
 }
+
--- binary-tree-pruning/src/test/scala/binarytreepruning/BinaryTreePruningSpec.scala
+++ binary-tree-pruning/src/test/scala/binarytreepruning/BinaryTreePruningSpec.scala
@@ -1,22 +1,34 @@
 package binarytreepruning
 
+import binarytreepruning.BinaryTreePruning
+import binarytreepruning.BinaryTreePruning.TreeNode
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
-import binarytreepruning.BinaryTreePruning
 import org.scalatest.prop.TableDrivenPropertyChecks._
-import binarytreepruning.BinaryTreePruning.TreeNode
+import org.scalatest.prop.TableFor2
 
 
 class BinaryTreePruningSpec extends AnyWordSpec with Matchers {
 
-  def TN = TreeNode.apply _
+  def T(v: Int, l: Option[TreeNode], r: Option[TreeNode]): Option[TreeNode] =
+    Some(TreeNode(v, l, r))
+  def R(v: Int, l: Option[TreeNode], r: Option[TreeNode]): TreeNode = T(v,l,r).get
+  val N: Option[TreeNode] = None
+  def L(v: Int): Option[TreeNode] = T(v, N, N)
 
   "Binary Tree Pruning" should {
     "compute correctly" in {
       val values =
         Table(
           ("input", "result"),
-          (TN(1,None,None), TN(1,None,None)),
+          (R(1,N,N), R(1,N,N)),
+          // test cases from the task definition
+          (R(1,N,T(0,L(0),L(1))),
+            R(1,N,T(0,N,L(1)))),
+          (R(1,T(0,L(0),L(0)), T(1,L(0),L(1))),
+            R(1,N,T(1,N,L(1)))),
+          (R(1,T(1,T(1,L(0),N),L(1)), T(0,L(0),L(1))),
+            R(1,T(1,L(1),L(1)), T(0,N,L(1)))),
         )
 
       forAll (values) {
@@ -28,3 +40,4 @@ class BinaryTreePruningSpec extends AnyWordSpec with Matchers {
     }
   }
 }
+
