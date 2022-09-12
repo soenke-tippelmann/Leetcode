e7a8b8053b2ac2c09534c9fbc9d3744cda24d306
Sönke Tippelmann <st@local>
2022-09-12 22:41:30 +0200

    Implement solution.

--- reverse-integer/src/main/scala/reverseinteger/ReverseInteger.scala
+++ reverse-integer/src/main/scala/reverseinteger/ReverseInteger.scala
@@ -3,6 +3,27 @@ package reverseinteger
 import scala.annotation.tailrec
 
 object ReverseInteger {
-  def run(number: Int): Boolean = ???
+  def run(number: Int): Int = {
+    if ( number == Int.MinValue ) 0
+    else if ( number < 0 ) -iterate(-number, 0)
+    else iterate(number, 0)
+  }
+
+  @tailrec
+  private def iterate(number: Int, result: Int): Int = {
+    if ( number == 0 ) {
+      return result
+    }
+
+    val digit = number % 10
+    val remainder = number / 10
+
+    val maximum = Int.MaxValue / 10
+    if ( result > maximum || 10 * result > Int.MaxValue - digit ) {
+        return 0
+    }
+
+    iterate(remainder, 10 * result + digit)
+  }
 }
 
--- reverse-integer/src/test/scala/reverseinteger/ReverseIntegerSpec.scala
+++ reverse-integer/src/test/scala/reverseinteger/ReverseIntegerSpec.scala
@@ -12,6 +12,17 @@ class ReverseIntegerSpec extends AnyWordSpec with Matchers {
       val values =
         Table(
           ("input", "result"),
+          (0, 0),
+          (1, 1),
+          (-1, -1),
+          (12, 21),
+          (-12, -21),
+          (Int.MaxValue, 0),
+          (2143443412, 2143443412),
+          (2143443491, 1943443412),
+          (2143443405, 0),
+          (Int.MinValue, 0),
+          (Int.MinValue + 1, 0),
           // test cases from the task definition
           (123, 321),
           (-123, -321),
