d51781254903c9f836a5ac04745acefec750b97a
Sönke Tippelmann <st@local>
2022-09-18 01:00:08 +0200

    Implement naive approach.

--- minimum-interval/src/main/scala/minimuminterval/MinimumInterval.scala
+++ minimum-interval/src/main/scala/minimuminterval/MinimumInterval.scala
@@ -1,6 +1,20 @@
 package minimuminterval
 
 object MinimumInterval {
-  def naive(intervals: Array[Array[Int]], queries: Array[Int]): Array[Int] = ???
+  case class Interval(left: Int, right: Int) {
+    val size = right - left + 1
+    def contains(value: Int) = value >= left && value <= right
+    override def toString = s"[$left,$right]"
+  }
+
+  def naive(intervals: Array[Interval], queries: Array[Int]): Array[Int] =
+    queries
+      .map( query => {
+        intervals
+          .filter(_.contains(query))
+          .map(_.size)
+          .minOption
+          .getOrElse(-1)
+      })
 }
 
--- minimum-interval/src/test/scala/minimuminterval/MinimumIntervalSpec.scala
+++ minimum-interval/src/test/scala/minimuminterval/MinimumIntervalSpec.scala
@@ -5,25 +5,27 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import minimuminterval.MinimumInterval
 import scala.reflect.ClassTag
+import minimuminterval.MinimumInterval.Interval
 
 class MinimumIntervalSpec extends AnyWordSpec with Matchers {
   def A[T: ClassTag](values: T*) = Array.apply(values:_*)
+  def I(left: Int, right: Int) = Interval(left, right)
 
   "Minimum interval" should {
     "compute correctly" in {
       val values =
         Table(
           ("intervals", "queries", "result"),
-          (A(A(1,3)), A(4), A(-1)),
-          (A(A(1,3)), A(2), A(3)),
+          (A(I(1,3)), A(4), A(-1)),
+          (A(I(1,3)), A(2), A(3)),
           // test cases from the task definition
-          (A(A(1,4),A(2,4),A(3,6),A(4,4)), A(2,3,4,5), A(3,3,1,4)),
-          (A(A(2,3),A(2,5),A(1,8),A(20,25)), A(2,19,5,22), A(2,-1,4,6))
+          (A(I(1,4),I(2,4),I(3,6),I(4,4)), A(2,3,4,5), A(3,3,1,4)),
+          (A(I(2,3),I(2,5),I(1,8),I(20,25)), A(2,19,5,22), A(2,-1,4,6))
         )
 
       forAll (values) {
-        (intervals: Array[Array[Int]], queries: Array[Int], result:Array[Int]) => {
-          println(s"\nRunning $intervals, $queries expecting $result")
+        (intervals: Array[Interval], queries: Array[Int], result:Array[Int]) => {
+          println(s"\nRunning [${intervals.mkString(", ")}], [${queries.mkString(",")}] expecting [${result.mkString(",")}]")
           MinimumInterval.naive(intervals, queries) shouldEqual result
         }
       }
