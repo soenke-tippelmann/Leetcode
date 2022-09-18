1a8c02a2eefc526319746bbfa9741fa4917e2f05
Sönke Tippelmann <st@local>
2022-09-18 02:30:23 +0200

    Implement optimized solution.

--- minimum-interval/src/main/scala/minimuminterval/MinimumInterval.scala
+++ minimum-interval/src/main/scala/minimuminterval/MinimumInterval.scala
@@ -1,5 +1,8 @@
 package minimuminterval
 
+import scala.collection.mutable.TreeMap
+import scala.annotation.tailrec
+
 object MinimumInterval {
   case class Interval(left: Int, right: Int) {
     val size = right - left + 1
@@ -16,5 +19,63 @@ object MinimumInterval {
           .minOption
           .getOrElse(-1)
       })
+
+  // -------------------------------------------
+
+  def optimized(intervals: Array[Interval], queries: Array[Int]): Array[Int] = {
+    val lookupTree = buildTree(intervals)
+    queries.map(findValue(lookupTree, _))
+  }
+
+  def buildTree(intervals: Array[Interval]): TreeMap[Int, Int] = {
+    val tree = TreeMap(0 -> -1)
+
+    @tailrec
+    def iterate(intervals: List[Interval]): Unit = {
+      intervals match {
+        case Nil => ()
+        case interval :: xs =>
+          val maxBeforeRight = findValue(tree, interval.right + 1)
+
+          tree.update(interval.right + 1, maxBeforeRight)
+
+          val maxBefore = findValue(tree, interval.left)
+
+          if(maxBefore > interval.size || maxBefore == -1) {
+            tree.update(interval.left, interval.size)
+          }
+
+          val entries = tree.range(interval.left + 1, interval.right + 1).toList
+
+          @tailrec
+          def it2(entries: List[(Int, Int)], prevValue: Int): Unit = {
+            entries match {
+              case Nil => ()
+              case (position, value) :: xs if value <= interval.size && value != -1 =>
+                it2(xs, value)
+              case (position, value) :: xs if prevValue < interval.size && prevValue != -1 =>
+                tree.update(position, interval.size)
+                it2(xs, interval.size)
+              case (position, _) :: xs =>
+                tree.remove(position)
+                it2(xs, prevValue)
+            }
+          }
+
+          it2(entries, findValue(tree, interval.left))
+
+          iterate(xs)
+      }
+    }
+
+    iterate(intervals.toList)
+
+    println(tree)
+
+    tree
+  }
+
+  def findValue(lookupTree: TreeMap[Int, Int], query: Int): Int =
+    lookupTree.get(query).getOrElse(lookupTree.maxBefore(query).get._2)
 }
 
--- minimum-interval/src/test/scala/minimuminterval/MinimumIntervalSpec.scala
+++ minimum-interval/src/test/scala/minimuminterval/MinimumIntervalSpec.scala
@@ -18,15 +18,28 @@ class MinimumIntervalSpec extends AnyWordSpec with Matchers {
           ("intervals", "queries", "result"),
           (A(I(1,3)), A(4), A(-1)),
           (A(I(1,3)), A(2), A(3)),
+          (A(I(1,2),I(5,7)), A(2,4,6,8), A(2,-1,3,-1)),
+          (A(I(2,6),I(4,5)), A(1,2,4,5,6,7), A(-1,5,2,2,5,-1)),
+          (A(I(2,6),I(5,7)), A(4,5,6,7,8), A(5,3,3,3,-1)),
+          (A(I(4,6),I(5,9)), A(4,5,6,7), A(3,3,3,5)),
           // test cases from the task definition
           (A(I(1,4),I(2,4),I(3,6),I(4,4)), A(2,3,4,5), A(3,3,1,4)),
           (A(I(2,3),I(2,5),I(1,8),I(20,25)), A(2,19,5,22), A(2,-1,4,6))
         )
 
+      println("Testing naive approach")
       forAll (values) {
         (intervals: Array[Interval], queries: Array[Int], result:Array[Int]) => {
           println(s"\nRunning [${intervals.mkString(", ")}], [${queries.mkString(",")}] expecting [${result.mkString(",")}]")
           MinimumInterval.naive(intervals, queries) shouldEqual result
+        }
+      }
+
+      println("Testing optimized approach")
+      forAll (values) {
+        (intervals: Array[Interval], queries: Array[Int], result:Array[Int]) => {
+          println(s"\nRunning [${intervals.mkString(", ")}], [${queries.mkString(",")}] expecting [${result.mkString(",")}]")
+          MinimumInterval.optimized(intervals, queries) shouldEqual result
         }
       }
     }
