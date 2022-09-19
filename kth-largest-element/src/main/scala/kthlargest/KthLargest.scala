115418e328227302124462ea05e1e91408081492
Sönke Tippelmann <st@local>
2022-09-19 19:34:29 +0200

    Implement solution.

--- kth-largest-element/src/main/scala/kthlargest/KthLargest.scala
+++ kth-largest-element/src/main/scala/kthlargest/KthLargest.scala
@@ -1,6 +1,18 @@
 package kthlargest
 
+import scala.collection.mutable.PriorityQueue
+
 class KthLargest(k: Int, nums: List[Int]) {
-  def add(value: Int): Int = ???
+  val ordering = Ordering.Int.reverse
+  val queue = PriorityQueue(nums.take(k):_*)(ordering)
+  nums.drop(k).foreach(add _)
+
+  def add(value: Int): Int = {
+    if(queue.head <= value) {
+      queue.dequeue()
+      queue.addOne(value)
+    }
+    queue.head
+  }
 }
 
