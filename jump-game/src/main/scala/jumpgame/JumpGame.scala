c89afa97d29bb86a76b9a2b6bee52867a1d1f78a
Sönke Tippelmann <st@local>
2022-09-19 18:42:25 +0200

    Initial solution.

--- jump-game/src/main/scala/jumpgame/JumpGame.scala
+++ jump-game/src/main/scala/jumpgame/JumpGame.scala
@@ -3,6 +3,12 @@ package jumpgame
 import scala.annotation.tailrec
 
 object JumpGame {
-  def run(input: Array[Int]): Boolean = ???
+  def run(input: Array[Int]): Boolean = iterate(input, 0)
+
+  private def iterate(input: Array[Int], position: Int): Boolean = {
+    if(position >= input.size) false
+    else if (position == input.size - 1) true
+    else List.range(1, input(position) + 1).exists(jumpSize => iterate(input, position + jumpSize))
+  }
 }
 
