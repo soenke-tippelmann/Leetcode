401d461d85628b2f8371657375a578c999bb3a24
Sönke Tippelmann <st@local>
2022-09-29 05:34:43 +0200

    Implement optimal solution.

--- top-k-frequent-elements/src/main/scala/topk/TopK.scala
+++ top-k-frequent-elements/src/main/scala/topk/TopK.scala
@@ -1,14 +1,101 @@
 package topk
 
+import scala.collection.mutable.{Map => mMap, Set => mSet}
+
 object TopK {
   def naive(numbers: Array[Int], k: Int): Array[Int] = {
     numbers
       .groupBy(identity)
       .mapValues(_.size)
       .toArray
+      .sortBy(_._1)(Ordering.Int.reverse) // just to stabilize the order to compare algorithms in the tests
       .sortBy(_._2)(Ordering.Int.reverse)
       .map(_._1)
       .take(k)
+  }
+
+
+  class Element(var count: Int, var pred: Option[Element], var succ: Option[Element], var numbers: mSet[Int]) {
+    def output: Unit = {
+      println(count, numbers, pred.map(_.count), succ.map(_.count))
+      succ.foreach(_.output)
+    }
+  }
+
+  def optimized(numbers: Array[Int], k: Int): Array[Int] = {
+    val lookup: mMap[Int, Element]  = mMap.empty
+
+    var minimum = new Element(1, None, None, mSet.empty)
+    var maximum = minimum
+
+    def addNumber(number: Int): Unit = {
+      val elementO = lookup.get(number)
+
+      if(elementO.isEmpty) {
+        if(minimum.count > 1) {
+          val newMinimum = new Element(1, None, Some(minimum), mSet.empty)
+          minimum.pred = Some(newMinimum)
+          minimum = newMinimum
+        }
+        lookup.addOne(number, minimum)
+        minimum.numbers.addOne(number)
+      } else {
+        val element = elementO.get
+
+        if(element.numbers.size == 1) {
+          if(element.succ.isDefined && element.succ.get.count == element.count + 1) {
+            element.succ.get.pred = element.pred
+            element.succ.get.numbers.addOne(number)
+            lookup.update(number, element.succ.get)
+            if(element.pred.isDefined) {
+              element.pred.get.succ = element.succ
+            }
+            if(minimum == element) {
+              minimum = element.succ.get
+            }
+          } else {
+            element.count += 1
+          }
+        } else {
+          if(element.succ.isDefined && element.succ.get.count == element.count + 1) {
+            element.numbers.subtractOne(number)
+            element.succ.get.numbers.addOne(number)
+            lookup.update(number, element.succ.get)
+          } else {
+            val newSucc = new Element(element.count + 1, Some(element), element.succ, mSet(number))
+            if(element.succ.isDefined) {
+              element.succ.get.pred = Some(newSucc)
+            }
+            element.succ = Some(newSucc)
+            element.numbers.subtractOne(number)
+            lookup.update(number, newSucc)
+            if(newSucc.succ.isEmpty) {
+              maximum = newSucc
+            }
+          }
+        }
+      }
+    }
+
+    def readTopK(count: Int, element: Element): Array[Int] = {
+      if(count > 0) {
+        if(element.pred.isDefined) {
+          element.numbers.toArray.sorted(Ordering.Int.reverse).take(count) ++ readTopK(count - element.numbers.size, element.pred.get)
+        } else {
+          element.numbers.toArray
+        }
+      } else {
+        Array.empty
+      }
+    }
+
+    numbers.foreach(number => {
+      //println("adding number", number)
+      addNumber(number)
+      //minimum.output
+    })
+    //minimum.output
+    readTopK(k, maximum)
   }
 }
 
--- top-k-frequent-elements/src/test/scala/topk/TopKSpec.scala
+++ top-k-frequent-elements/src/test/scala/topk/TopKSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import topk.TopK
 import scala.reflect.ClassTag
+import scala.util.Random
 
 class TwoSum2Spec extends AnyWordSpec with Matchers {
   def A[T: ClassTag](values: T*) = Array.apply(values:_*)
@@ -25,6 +26,27 @@ class TwoSum2Spec extends AnyWordSpec with Matchers {
         (numbers: Array[Int], k: Int, result: Array[Int]) => {
           println(s"\nRunning ${toStr(numbers)} with k=$k expecting ${toStr(result)}")
           TopK.naive(numbers, k) should contain theSameElementsAs result
+          TopK.optimized(numbers, k) should contain theSameElementsAs result
+        }
+      }
+
+      val randomValues =
+        Table(
+          ("numbers", "k"),
+          (Array.tabulate(10)(_ => Random.nextInt() % 2 - 1), 5),
+          (Array.tabulate(100)(_ => Random.nextInt() % 2 - 1), 5),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 5),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 5),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 10),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 20),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 50),
+          (Array.tabulate(1000)(_ => Random.nextInt() % 20 - 10), 1000)
+        )
+
+      forAll (randomValues) {
+        (numbers: Array[Int], k: Int) => {
+          println(s"\nRunning random sequence of size ${numbers.size} with k=$k")
+          TopK.naive(numbers, k) should contain theSameElementsAs TopK.optimized(numbers, k)
         }
       }
     }
